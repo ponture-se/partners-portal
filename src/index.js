b901c1126db4edabc41c3251d8294169d49d5df8
saeedPadyab <padyabsaeed@gmail.com>
2020-04-20 12:38:19 +0300

    remove live view

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 //
 import store from "./services/redux/store";
 import { setAppLang } from "./services/languageManager";
-import Storage from "./services/storageManager";
 import setAuthorizationToken from "./utils/setAuthorizationToken";
 import { setAuthorization, setUser } from "./services/redux/auth/actions";
-import liveView from "./services/liveView";
-liveView().start();
+
 // app language
 const pathName = window.location.pathname;
 const lang = pathName.split("/")[1];
@@ -37,8 +34,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
